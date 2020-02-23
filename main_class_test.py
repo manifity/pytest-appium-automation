c1e8d195939382da2d4598d17d8a1824f75b61a2
manifity <manifity@gmail.com>
2020-02-23 18:54:35 +0300

    The second test.

--- main_class_test.py
+++ main_class_test.py
@@ -5,3 +5,6 @@ class TestMainClassTest:
 
     def test_get_local_number(self):
         assert MainTest.get_local_number() == 14, "Число != 14"
+
+    def test_get_class_number(self):
+        assert MainTest.get_class_number() > 45, "Число < 45"
--- main_test.py
+++ main_test.py
@@ -1,5 +1,11 @@
 class MainTest:
 
+    _class_number = 20
+
     @staticmethod
     def get_local_number():
         return 14
+
+    @staticmethod
+    def get_class_number():
+        return MainTest._class_number
