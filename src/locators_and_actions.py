90f704345ef8fc8dcc7907501ccf256dc9df6aa9
manifity <manifity@gmail.com>
2020-03-10 17:03:44 +0300

    Ex3: Тест: отмена поиска

--- src/locators_and_actions.py
+++ src/locators_and_actions.py
@@ -11,7 +11,23 @@ class OnboardingPage(BasePage):
 
 
 class MainElements(BasePage):
-    _search_field = (MobileBy.XPATH, '//*[contains(@text, "Поиск по Википедии")]')
+    _search_field_default_text = (MobileBy.XPATH, '//*[contains(@text, "Поиск по Википедии")]')
+    _search_field_id = (MobileBy.ID, '%s:id/search_container' % BUNDLE_APP)
+    _search_text_field_container_id = (MobileBy.ID, '%s:id/search_src_text' % BUNDLE_APP)
+    _cancel_search_button = (MobileBy.ID, '%s:id/search_close_btn' % BUNDLE_APP)
+    _empty_message_string = (MobileBy.ID, '%s:id/search_empty_message' % BUNDLE_APP)
 
     def get_text_in_the_search_field(self):
-        return super().get_element_text(self._search_field)
+        return super().get_element_text(self._search_field_default_text)
+
+    def click_to_the_search_field(self):
+        super().get_element_and_click(self._search_field_default_text)
+
+    def input_text_to_the_search_field(self, text):
+        super().get_element(self._search_text_field_container_id).send_keys(text)
+
+    def press_to_cancel_search_button(self):
+        super().get_element_and_click(self._cancel_search_button)
+
+    def get_text_in_the_empty_search(self):
+        return super().get_element_text(self._empty_message_string)
--- tests/test_first_test.py
+++ tests/test_first_test.py
@@ -6,3 +6,19 @@ def test_get_text_from_search_field(appdriver):
 
     assert MainElements(appdriver).get_text_in_the_search_field() == 'Поиск по Википедии',\
         'Text isn\'t presented in the Search field'
+
+
+def test_cancel_search(appdriver):
+    OnboardingPage(appdriver).press_skip_onboarding_button()
+    MainElements(appdriver).click_to_the_search_field()
+    MainElements(appdriver).input_text_to_the_search_field('Python')
+
+    assert appdriver.find_element_by_android_uiautomator('new UiSelector().text("Python")').text == 'Python', \
+        'Search results is different, than request'
+
+    MainElements(appdriver).press_to_cancel_search_button()
+
+    assert MainElements(appdriver).get_text_in_the_search_field() == 'Поиск по Википедии',\
+        'Text isn\'t presented in the Search field'
+    assert MainElements(appdriver).get_text_in_the_empty_search() == \
+        'Ищите и читайте свободную энциклопедию на своём языке', 'Search results aren\'t vanished'
